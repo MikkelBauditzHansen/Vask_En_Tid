d22388a7918666b73843fb1794d365fb601d7f59
Mikkel Bauditz Hansen <mih016@edu.zealand.dk>
2025-10-07 09:27:15 +0200

    sdfghlæ

---
+++ Vask_En_Tid_Library/Models/ConcreteMachine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vask_En_Tid_Library.Models
+{
+    internal class ConcreteMachine : Machine
+    {
+        public ConcreteMachine(int id, int bookingId, string machineType, string name)
+            : base(id, bookingId, machineType, name)
+        {
+        }
+
+        public override void Book()
+        {
+            Console.WriteLine($"🧠 Maskine '{MachineName}' af typen '{MachineType}' er nu booket!");
+        }
+
+        public override void CancelBooking()
+        {
+            Console.WriteLine($"🧠 Booking af maskine '{MachineName}' (type: {MachineType}) er annulleret.");
+        }
+        public ConcreteMachine() { }
+    }
+}
--- Vask_En_Tid_Library/Repository/MachineCollectionRepo.cs
+++ Vask_En_Tid_Library/Repository/MachineCollectionRepo.cs
@@ -22,13 +22,13 @@ namespace Vask_En_Tid_Library.Repository
             var machines = new List<Machine>();
             using (var connection = new SqlConnection(_connectionString))
             {
-                var command = new SqlCommand("SELECT MachineID, BookingDate, ResidentID, MachineID, TimeSlot FROM Bookings", connection);
+                var command = new SqlCommand("SELECT MachineID, BookingID, MachineName, MachineType FROM Machines", connection);
                 connection.Open();
                 using (var reader = command.ExecuteReader())
                 {
                     while (reader.Read())
                     {
-                        var Machine = new Machine
+                        var machine = new ConcreteMachine
                         {
                             MachineID = (int)reader["MachineID"],
                             BookingID = (int)reader["BookingID"],
@@ -50,6 +50,30 @@ namespace Vask_En_Tid_Library.Repository
                 command.Parameters.AddWithValue("@BookingID", machine.BookingID);
                 command.Parameters.AddWithValue("@MachineName", machine.MachineName);
                 command.Parameters.AddWithValue("@MachineType", machine.MachineType);
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void Update(Machine machine)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var command = new SqlCommand("UPDATE Machines SET MachineID = @MachineID, BookingID = @BookingID, MachineName = @MachineName, MachineType = @MachineType WHERE MachineID = @MachineID", connection);
+                command.Parameters.AddWithValue("@MachineID", machine.MachineID);
+                command.Parameters.AddWithValue("@BookingID", machine.BookingID);
+                command.Parameters.AddWithValue("@MachineName", machine.MachineName);
+                command.Parameters.AddWithValue("@MachineType", machine.MachineType);
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+        public void Delete(int id)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var command = new SqlCommand("DELETE FROM Machines WHERE MachineID = @MachineID", connection);
+                command.Parameters.AddWithValue("@MachineID", id);
                 connection.Open();
                 command.ExecuteNonQuery();
             }
